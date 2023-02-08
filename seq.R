9c53126aeb1bf52d8faa824e00639518daafc858
Sanchit Dass <sanchitd9@gmail.com>
2023-02-07 17:59:28 -1000

    blah

--- seq.R
+++ seq.R
@@ -53,13 +53,10 @@ q2
 
 attributes(q2)
 
-q2$req[[4]]
+dengueSeq <- getSequence(q2$req[[1]])
 
 
 closebank()
-
-
-
 
 
 getncbiseq <- function(accession) {
@@ -76,7 +73,7 @@ getncbiseq <- function(accession) {
     if (!(inherits(resquery, "try-error"))) {
       queryname <- "query2"
       thequery <- paste("AC=", accession, sep="")
-      query(queryname, thequery)
+      query2 <- query(queryname, thequery)
       seq <- getSequence(query2$req[1])
       closebank()
       return (seq)
@@ -88,3 +85,68 @@ getncbiseq <- function(accession) {
 
 
 x <- getncbiseq("NC_001477")
+
+
+
+# Class work - 3rd Feb
+
+d1 <- list(getSequence(q2$req[[1]]))
+d2 <- list(getSequence(q2$req[[2]]))
+d3 <- list(getSequence(q2$req[[3]]))
+d4 <- list(getSequence(q2$req[[4]]))
+
+dengue <- getSequence(q2)
+
+write.fasta(dengue, c("M1", "M2", "M3", "M4"), file.out = "my_output.fasta")
+
+temp <- d1[[1]]
+length(temp)
+
+bases <- table(d1)
+
+barplot(bases)
+
+
+slidingwindowplot <- function(windowsize, inputseq) {
+  starts <- seq(1, length(inputseq)-windowsize, by = windowsize)
+  n <- length(starts)
+  chunkGCs <- numeric(n)
+  for (i in 1:n) {
+    chunk <- inputseq[starts[i]:(starts[i]+windowsize-1)]
+    chunkGC <- GC(chunk)
+    print(chunkGC)
+    chunkGCs[i] <- chunkGC
+  }
+  plot(starts,chunkGCs,type="b",xlab="Nucleotide start position",ylab="GC content")
+}
+
+
+slidingwindowplot(2000, d1[[1]])
+
+
+myTable = count(d1[[1]], 2)
+myTable
+
+class(myTable)
+names(myTable)
+
+pie(myTable, labels = names(myTable))
+
+
+# repr <- function (seq, pattern) {
+#   count(seq, 2)["ta"] / (count(seq, 1)["t"] * count(seq, 1)["a"])  
+# }
+
+count(d1[[1]], 2)["ta"] / (count(d1[[1]], 1)["t"] * count(d1[[1]], 1)["a"])
+
+count(d1[[1]], 2)["cg"] / (count(d1[[1]], 1)["c"] * count(d1[[1]], 1)["g"])
+
+
+l <- length(d1[[1]])
+(count(d1[[1]], 2)["cg"] / l) / ((count(d1[[1]], 1)["c"] / l) * (count(d1[[1]], 1)["g"] / l))
+
+s <- sample(c("a", "c", "g", "t"), 1000, replace = TRUE, prob = c(0.2, 0.3, 0.3, 0.2))
+s
+
+prob <- table(s) / sum(table(s))
+prob
