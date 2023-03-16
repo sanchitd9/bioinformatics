a58a365a6c7b9b99856067159e8288ba81a31f94
Sanchit Dass <sanchitd9@gmail.com>
2023-03-16 19:03:19 -0400

    fixed question 2

--- hw4/hw4.R
+++ hw4/hw4.R
@@ -1,6 +1,8 @@
 require(seqinr)
 require(Biostrings)
 require(ORFhunteR)
+require(ggplot2)
+require(gridExtra)
 
 # Setup
 choosebank("genbank")
@@ -19,9 +21,22 @@ seq2 <- getSequence(q2$req[[1]])
 print(DNAString(c2s(seq1)))
 print(DNAString(c2s(seq2)))
 
+#-----------------------------------------#
+# Use this if connection is broken
+
+# q1 <- read.fasta(file = paste("hw4/", ac_1, ".fasta", sep = ""))
+# q2 <- read.fasta(file = paste("hw4/", ac_2, ".fasta", sep = ""))
+# 
+# seq1 <- getSequence(q1)[[1]]
+# seq2 <- getSequence(q2)[[1]]
+#-----------------------------------------#
+
 # Task B
-seq1_count <- table(seq1)
-seq2_count <- table(seq2)
+p1 <- seqinr::translate(seq1)
+p2 <- seqinr::translate(seq2)
+
+seq1_count <-table(p1)
+seq2_count <- table(p2)
 print(seq1_count)
 print(seq2_count)
 
@@ -30,18 +45,13 @@ seq2_prop <- proportions(seq2_count)
 print(seq1_prop)
 print(seq2_prop)
 
-colors1 <- c("#edbc72", "#77b072", "#35689c", "#f095c5")
-colors2 <- c("#7fb6ba", "#6e1930", "#990681", "#dfe866")
-
-par(mfrow = c(1, 2))
-pie(seq1_prop, labels = paste(names(seq1_prop), "=", round(seq1_prop * 100, 3), "%"), col = colors1, main = ac_1)
-pie(seq2_prop, labels = paste(names(seq2_prop), "=", round(seq2_prop * 100, 3), "%"), col = colors2, main = ac_2)
+df1 <- data.frame(slices = seq1_prop, "amino_acids" = paste(names(seq1_prop), "=", round(seq1_prop * 100, 2), "%", sep = ""))
+df2 <- data.frame(slices = seq2_prop, amino_acids = paste(names(seq2_prop), "=", round(seq2_prop * 100, 2), "%", sep = ""))
+plot1 <- ggplot(df1, aes(x = "", y = slices.Freq, fill = amino_acids)) + geom_bar(stat = "identity", width = 1) + coord_polar("y", start = 0) + ggtitle(ac_1) + xlab("") + ylab("")
+plot2 <- ggplot(df2, aes(x = "", y = slices.Freq, fill = amino_acids)) + geom_bar(stat = "identity", width = 1) + coord_polar("y", start = 0) + ggtitle(ac_2) + xlab("") + ylab("")
+grid.arrange(plot1, plot2, ncol = 2)
 
 # Task C
-seq1_orfs <- findORFs(c2s(seq1))
-seq2_orfs <- findORFs(c2s(seq2))
-
-par(mfrow = c(1, 1))
 dotPlot(s2c(seq1_orfs[1, "orf.sequence"]), s2c(seq2_orfs[1, "orf.sequence"]), xlab = ac_1, ylab = ac_2)
 
 # Task D
@@ -70,3 +80,5 @@ print(p_value)
 # Task F
 opt_la <- pairwiseAlignment(DNAString(c2s(seq1)), DNAString(c2s(seq2)), type = "local", substitutionMatrix = nucleotideSubstitutionMatrix(match = 3, mismatch = -2, baseOnly = TRUE), gapOpening = -4, gapExtension = -2)
 print(opt_la)
+
+closebank()
